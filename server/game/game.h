9224a914ada09716c7eb7df0be85498868d7f184
Juho <juho.rantala@live.com>
2022-02-07 00:11:09 +0200

    Public interface to Game

--- server/game/game.h
+++ server/game/game.h
@@ -15,15 +15,30 @@ public:
     Game(EventHandler* eventHandler);
     ~Game();
 
+    // Players
     bool add(id id) override;
     bool add(Member* member) override;
     Player* getPlayer(id id);
+    std::vector<Player*> getPlayers();
+    Player* changeTurn();
+
+    // Game
+    void initGame();
+
+    // Deck
+    void draw(Player* player);
+    void draw(Player* player, int amount);
+    Card playFromDeck(Player* player);
+
+    // CardStack
+    void takeLatest(Player* player);
 
 private:
 
     EventHandler* eventHandler_;
     Deck* deck_;
     CardStack* cardStack_;
+    Player* inTurn;
 };
 
 #endif // GAME_H
