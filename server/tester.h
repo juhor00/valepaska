5a87fdb32988b8e7fae201396d65f551d0bf594b
Juho <juho.rantala@live.com>
2022-02-08 21:16:37 +0200

    Added prompts and Exception class

--- server/tester.h
+++ server/tester.h
@@ -6,8 +6,19 @@
 #include <iostream>
 #include <iomanip>
 #include <map>
+#include <algorithm>
 
 using namespace std;
+
+
+class BackException : public exception {
+public:
+
+    BackException(const exception& e):
+        exception(e){}
+    BackException():
+        exception(){}
+};
 
 class Tester
 {
@@ -23,11 +34,20 @@ private:
     void printHelp();
     int calcLongestCmd();
 
+    id promptPlayer();
+    cards promptCards(id player);
+    int promptClaim();
+
+
+
     Game* game_;
     map<string, string> help_ = {
         {"?", "Prints all commands and their description"},
         {"quit", "Quits the program"},
         {"print", "Prints the game state"},
+        {"play", "Prompts for the player and cards to be played"},
+        {"deck", "Prompts for the player to play from deck"},
+
     };
     const int cmdLen_;
 };
