b75ac8a53e4eabb6b2ccdc5624449a56d5ba61e1
Juho <juho.rantala@live.com>
2022-03-08 19:15:38 +0200

    Update mainwindow.cpp

--- client/gui/mainwindow.cpp
+++ client/gui/mainwindow.cpp
@@ -4,19 +4,21 @@
 MainWindow::MainWindow(QWidget *parent):
     QMainWindow(parent),
     ui(new Ui::MainWindow),
-    hand_(new Hand)
+    hand_(new Hand(this))
 {
     ui->setupUi(this);
 
     ui->bottom->addWidget(hand_);
 
 
-    hand_->add(new OpenedCard(3, 'S'));
-    hand_->add(new OpenedCard(3, 'S'));
-    hand_->add(new OpenedCard(3, 'S'));
-    hand_->add(new OpenedCard(3, 'S'));
-    hand_->add(new OpenedCard(3, 'S'));
-    hand_->add(new OpenedCard(3, 'S'));
+    hand_->add(new OpenedCard(3, 'S', hand_));
+    hand_->add(new OpenedCard(3, 'S', hand_));
+    hand_->add(new OpenedCard(3, 'S', hand_));
+    hand_->add(new OpenedCard(3, 'S', hand_));
+    hand_->add(new OpenedCard(3, 'S', hand_));
+    hand_->add(new OpenedCard(3, 'S', hand_));
+
+    hand_->show();
 
 
 
